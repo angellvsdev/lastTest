99b8b64db21d892c3bb9522114a6cc239aa45f91
Angel Leonardo <angellvs71@gmail.com>
2022-11-17 09:30:00 -0400

    Añadidura de clase y creación de una primera instancia

---
+++ app.js
@@ -0,0 +1,8 @@
+class Pokemon {
+    constructor(mote, evolutions) {
+        this.mote = mote
+        this.evolutions = evolutions
+    }
+}
+
+let charmander = new Pokemon("Charmander", ["Charmeleon", "Charizard"])
