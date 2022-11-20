cda2907b08f6611702701027d2b6327c5492f339
Angel Leonardo <angellvs71@gmail.com>
2022-11-19 22:02:41 -0400

    Commit de reapertura

---
+++ app.js
@@ -0,0 +1,9 @@
+class Animal {
+    constructor(mote, age) {
+        this.mote = mote
+        this.age = age
+    }
+}
+
+let Yankee = new Animal("Yankee", 4)
+let Corina = new Animal("Corina", 5)
---
+++ modules.js
@@ -0,0 +1,8 @@
+class gardeningTool {
+    constructor(tool, gardeningUse, weight, durability) {
+        this.tool = tool
+        this.gardeningUse = gardeningUse
+        this.weight = weight
+        this.durability = durability
+    }
+}
